f06f6bcbb9630135d4d3a4c62beaa315c937a3a4
Misheck12 <misheck1720@gmail.com>
2022-10-13 10:35:12 +0200

    update speakers

--- speakers.js
+++ speakers.js
@@ -3,34 +3,18 @@ const speakersSection = document.querySelector('#speakers');
 const speakersObject = [
   {
     image: 'images/morgan01.jpg',
-    name: 'H.E Hakainde Hichilema',
-    about: 'Current president of Zambia',
+    name: 'Morgan Mbulo',
+    about: 'Founder at IMC Studios',
     history:
-      'He has been in opposition for 20 years failed 4 times. he owns shares in big youth companies.',
+      'Morgan Mbulo is a creative producer, currently serving as Vice President of NAMA. He loves to develop young minds in Arts.',
   },
 
   {
     image: 'images/misheck01.jpg',
-    name: 'H.E Nana Akufo-Addo',
-    about: 'Current president of Ghanan',
-    history: 'Ghanaian president who has served Ghana since 7 January 2017. In 2020, he was re-elected for his second term, which will end on 6 January 2025.',
+    name: 'Misheck Livingi',
+    about: 'Creative Enterpreneur',
+    history: 'Creative Enterpreneuar who is very passionate about youth development. he believes success is for anyone willing to have it.',
   },
-
-  // {
-  //   image: 'images/Paul_Kagame_2014.jpg',
-  //   name: 'H.E Paul Kagame ',
-  //   about: 'Current president of Rwanda',
-  //   history:
-  //     'Rwandan president and former military leader. He is the sixth president of Rwanda, having taken office in 2000.',
-  // },
-
-  // {
-  //   image: 'images/Paul_Kagame_2014.jpg',
-  //   name: 'H.E Paul Kagame ',
-  //   about: 'Current president of Rwanda',
-  //   history:
-  //     'Rwandan president and former military leader. He is the sixth president of Rwanda, having taken office in 2000.',
-  // },
 
 ];
 
